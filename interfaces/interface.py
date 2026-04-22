4abd56015d290f0377d7b29b985405a2e4be5128
Eliezer Carvalho <163750904+eliezerzx@users.noreply.github.com>
2026-04-22 14:23:18 -0300

    Add option to remove discounts in the menu

--- interfaces/interface.py
+++ interfaces/interface.py
@@ -12,6 +12,8 @@ def exibir_menu():
 ||  [2] Criar Complementos                      ||
 ||  [3] 2 Sabores                               ||
 ||  [4] Aplicar Descontos                       ||
+||  [5] Remover Descontos                       ||
+||  [6] Remover Descontos & Alterar Valor       ||                    
 ||  [9] Pegar Coordenada                        ||
 ||  [0] Sair do programa                        ||
 ||                                              ||
